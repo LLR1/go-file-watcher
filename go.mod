1b24ab0fbcf969a5c2fd05b614eaba0622c01f63
LLR1 <alexmadov27@gmail.com>
2025-03-24 16:01:03 +0100

    Initial commit: add basic file watcher

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/LLR1/go-file-watcher
+
+go 1.24.1
+
+require (
+	github.com/fsnotify/fsnotify v1.8.0 // direct
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	// Read directory path from flag, default = current directory
+	dir := flag.String("path", ".", "Directory to watch")
+	flag.Parse()
+
+	// Create watcher
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalf("Failed to create watcher: %v", err)
+	}
+	defer watcher.Close()
+
+	// Add directory
+	if err := watcher.Add(*dir); err != nil {
+		log.Fatalf("Failed to watch directory %s: %v", *dir, err)
+	}
+	fmt.Printf("Watching directory: %s\n", *dir)
+
+	// Setup logging: both stdout and file
+	logFile, err := os.OpenFile("tasks.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Unable to open log file: %v", err)
+	}
+	defer logFile.Close()
+	logger := log.New(io.MultiWriter(os.Stdout, logFile), "", log.LstdFlags)
+
+	// Handle events & errors
+	for {
+		select {
+		case event := <-watcher.Events:
+			logger.Println("Event:", event)
+		case err := <-watcher.Errors:
+			logger.Println("Error:", err)
+		}
+	}
+}
